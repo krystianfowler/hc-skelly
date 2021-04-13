b2eff55bce5b366fb202ad567f4d580c63f37ec7
nirzohu <canit3gw@anonaddy.me>
2021-04-13 12:02:25 +0200

    Implement auth app routing

--- src/authenticated-app.js
+++ src/authenticated-app.js
@@ -1,7 +1,30 @@
 import * as React from 'react'
+import {Switch, Route, Redirect} from 'react-router-dom'
 
 function AuthenticatedApp() {
-  return <h1>I am the AuthenticatedApp</h1>
+  return (
+    <Switch>
+      <Route exact path="/select" component={SelectScreen} />
+      <Route exact path="/dashboard">
+        <StatusScreen />
+        <ProgramScreen />
+      </Route>
+      <Route>
+        <Redirect to="/select" />
+      </Route>
+    </Switch>
+  )
+}
+
+function SelectScreen() {
+  return <h1>ApplianceSelectScreen</h1>
+}
+
+function StatusScreen() {
+  return <h1>StatusScreen</h1>
+}
+function ProgramScreen() {
+  return <h1>ProgramScreen</h1>
 }
 
 export default AuthenticatedApp
